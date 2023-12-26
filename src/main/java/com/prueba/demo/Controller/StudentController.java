7ada8dd6b690bf5723cd34dfdf1825f241c9e9e4
Ariel <vilcheariel2@gmail.com>
2023-12-25 21:51:39 -0300

    up 1.9

--- src/main/java/com/prueba/demo/Controller/StudentController.java
+++ src/main/java/com/prueba/demo/Controller/StudentController.java
@@ -2,6 +2,7 @@ package com.prueba.demo.Controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,11 +12,8 @@ import com.prueba.demo.Entities.StudentService;
 
 @Controller
 public class StudentController {
+    @Autowired
     private StudentService studentService;
-
-    public StudentController(StudentService studentService) {
-        this.studentService = studentService;
-    }
 
     @GetMapping("/")
     @ResponseBody
--- src/main/java/com/prueba/demo/Entities/Student.java
+++ src/main/java/com/prueba/demo/Entities/Student.java
@@ -2,6 +2,7 @@ package com.prueba.demo.Entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -9,7 +10,7 @@ import jakarta.persistence.Table;
 @Table
 public class Student {
     @Id
-    @GeneratedValue(generator = "uuid")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Integer age;
