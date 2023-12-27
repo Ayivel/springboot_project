7acb7f5079eee2410093c28868145f4059c2adb2
Ariel <vilcheariel2@gmail.com>
2023-12-27 19:51:40 -0300

    Creating index.html and MainController. V1.0

--- src/main/java/com/NEWS/news/Entities/New.java
+++ src/main/java/com/NEWS/news/Entities/New.java
@@ -3,6 +3,49 @@ package com.NEWS.news.Entities;
 public class New {
 
     private Long id;
-    private String titel;
+    private String title;
+    private String body;
+    private Image image;
+
+    public New() {
+    }
+
+    public New(String title, String body, Image image) {
+        this.title = title;
+        this.body = body;
+        this.image = image;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
 
 }
